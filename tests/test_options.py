407abbb70af70de8907d8849139edc1cd607e845
sralloza <sralloza@gmail.com>
2019-04-14 19:29:52 +0200

    Added tests for options

---
+++ tests/test_options.py
@@ -0,0 +1,25 @@
+import os
+
+from vcd import Options
+
+
+def test_filename_pattern():
+    pat = Options.FILENAME_PATTERN
+    assert pat.search('filename="federico.txt"').group(1) == 'federico.txt'
+    assert pat.search('filename="a la meca.rar"').group(1) == 'a la meca.rar'
+    assert not pat.search('filename="dummy.txt')
+
+
+def test_set_root_folder():
+    root_folder = Options.ROOT_FOLDER
+    temp_folder = 'temp-root-folder-to-delete'
+
+    assert not os.path.isdir(temp_folder)
+    Options.set_root_folder(temp_folder)
+    assert os.path.isdir(temp_folder)
+
+    Options.set_root_folder(root_folder)
+    assert os.path.isdir(root_folder)
+
+    os.rmdir(temp_folder)
+    assert not os.path.isdir(temp_folder)
