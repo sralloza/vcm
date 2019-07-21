92bc8f3a31ab3e58dc88d1cf5cdad32ca46e0dc5
sralloza <sralloza@gmail.com>
2019-07-21 18:14:57 +0200

    Create template for tests of threading

---
+++ tests/_threading.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+class TestWorker:
+    @pytest.mark.skip
+    def test_attributes(self):
+        pass
+
+    @pytest.mark.skip
+    def test_to_log(self):
+        pass
+
+    @pytest.mark.skip
+    def test_run(self):
+        pass
+
+
+class TestKiller:
+    @pytest.mark.skip
+    def test_attributes(self):
+        pass
+
+    @pytest.mark.skip
+    def test_to_log(self):
+        pass
+
+    @pytest.mark.skip
+    def test_run(self):
+        pass
+
+
+@pytest.mark.skip
+def test_start_workers():
+    pass
