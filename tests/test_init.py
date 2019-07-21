3398e95b00846da4029307fc83a27719237ab098
sralloza <sralloza@gmail.com>
2019-07-21 18:10:08 +0200

    Create template of tests of init

---
+++ tests/test_init.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.mark.skip
+def test_logging_setup():
+    pass
+
+@pytest.mark.skip
+def test_find_subjects():
+    pass
+
+
+@pytest.mark.skip
+def test_start():
+    pass
