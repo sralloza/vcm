64aca3e27e9a137b7798321732b51f9cce0d1687
sralloza <sralloza@gmail.com>
2019-07-21 19:12:50 +0200

    Create template for tests of exceptions

---
+++ tests/test_exceptions.py
@@ -0,0 +1,60 @@
+def test_base_vcd_exception():
+    pass
+
+
+class TestDownloaderExceptions:
+    def test_base_downloader_error(self):
+        pass
+
+    def test_downloader_error(self):
+        pass
+
+
+class TestCredentialExceptions:
+    def test_base_credential_error(self):
+        pass
+
+    def test_credential_error(self):
+        pass
+
+    def test_no_credentials_found_error(self):
+        pass
+
+    def test_id_error(self):
+        pass
+
+
+class TestAliasExceptions:
+    def test_base_alias_error(self):
+        pass
+
+    def test_alias_not_found_error(self):
+        pass
+
+    def test_alias_fatal_error(self):
+        pass
+
+
+class TestFileCacheExceptions:
+    def test_base_file_cache_error(self):
+        pass
+
+    def test_file_cache_error(self):
+        pass
+
+
+class TestTimeOperationExceptions:
+    # todo rename exception
+    def test_base_time_operation_exception(self):
+        pass
+
+    def test_invalid_language_error(self):
+        pass
+
+
+class TestOptionExceptions:
+    def test_base_option_error(self):
+        pass
+
+    def test_option_error(self):
+        pass
