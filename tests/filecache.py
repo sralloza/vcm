d0e34083d42404d3cde48d78bf1db2564f354646
sralloza <sralloza@gmail.com>
2019-07-21 18:29:33 +0200

    Create template for tests of filecache

---
+++ tests/filecache.py
@@ -0,0 +1,42 @@
+import pytest
+
+
+class TestExceptions:
+    @pytest.mark.skip
+    def test_file_cache_error(self):
+        pass
+
+
+class TestFileCache:
+    @pytest.mark.skip
+    def test_attributes(self):
+        pass
+
+    @pytest.mark.skip
+    def test_contains(self):
+        pass
+
+    @pytest.mark.skip
+    def test_getitem(self):
+        pass
+
+    @pytest.mark.skip
+    def test_setitem(self):
+        pass
+
+    @pytest.mark.skip
+    def test_len(self):
+        pass
+
+    @pytest.mark.skip
+    def test_load(self):
+        pass
+
+    @pytest.mark.skip
+    def test_get_file_length(self):
+        pass
+
+
+@pytest.mark.skip
+def test_module_attributes_setup():
+    pass
