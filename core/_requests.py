8387071b6d2448651a3996195fa1f510e2706a57
sralloza <sralloza@gmail.com>
2019-07-21 18:52:48 +0200

    Move request's exceptions to .exceptions

--- core/_requests.py
+++ core/_requests.py
@@ -6,12 +6,10 @@ import logging
 
 import requests
 
+from .exceptions import DownloaderError
+
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
                          '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-
-
-class DownloaderError(Exception):
-    """Error while downloading."""
 
 
 class Downloader(requests.Session):
--- core/exceptions.py
+++ core/exceptions.py
@@ -1,6 +1,9 @@
-
 class CredentialError(Exception):
     """Credential error."""
+
+
+class DownloaderError(Exception):
+    """Error while downloading."""
 
 
 class NoCredentialsFoundError(Exception):
