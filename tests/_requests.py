966b5a21de6ea531142823fed2fd6c38055aab64
sralloza <sralloza@gmail.com>
2019-07-21 18:14:26 +0200

    Create template for tests of requests

---
+++ tests/_requests.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.mark.skip
+def test_headers():
+    pass
+
+
+@pytest.mark.skip
+def test_downloader_error():
+    pass
+
+
+class TestDownloader:
+    @pytest.mark.skip
+    def test_attributes(self):
+        pass
+
+    @pytest.mark.skip
+    def test_get(self):
+        pass
+
+    @pytest.mark.skip
+    def test_post(self):
+        pass
