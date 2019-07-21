0eaf2230b6b971b347c03b17d0c2ff5866b55599
sralloza <sralloza@gmail.com>
2019-07-21 18:16:35 +0200

    Create template for tests of credentials

---
+++ tests/credentials.py
@@ -0,0 +1,51 @@
+import pytest
+
+
+class TestExceptions:
+    @pytest.mark.skip
+    def test_credential_error(self):
+        pass
+
+    @pytest.mark.skip
+    def test_no_credentials_found_error(self):
+        pass
+
+
+class StudentCredentials:
+    @pytest.mark.skip
+    def test_attributes(self):
+        pass
+
+    @pytest.mark.skip
+    def test_str(self):
+        pass
+
+    @pytest.mark.skip
+    def test_to_json(self):
+        pass
+
+
+class Credentials:
+    @pytest.mark.skip
+    def test_attributes(self):
+        pass
+
+    @pytest.mark.skip
+    def test_load(self):
+        pass
+
+    @pytest.mark.skip
+    def test_save(self):
+        pass
+
+    @pytest.mark.skip
+    def test_get(self):
+        pass
+
+    @pytest.mark.skip
+    def test_make_example(self):
+        pass
+
+    @pytest.mark.skip
+    def add(self):
+        pass
