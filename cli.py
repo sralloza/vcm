3f6d90f176f74e7d0511e215d47fd4198ec30570
sralloza <sralloza@gmail.com>
2019-09-14 22:55:10 +0200

    Remove root-folder argument from CLI

--- cli.py
+++ cli.py
@@ -1,12 +1,10 @@
 import argparse
 
 from vcd import start
-from vcd._requests import Connection
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--root-folder', default=None)
     parser.add_argument('--nthreads', default=None, type=int)
     parser.add_argument('--no-killer', action='store_true')
     parser.add_argument('-d', '--debug', action='store_true')
@@ -19,4 +17,4 @@ if __name__ == '__main__':
         chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
         webbrowser.get(chrome_path).open_new('localhost')
 
-    start(root_folder=opt.root_folder, nthreads=opt.nthreads, no_killer=opt.no_killer)
+    start(nthreads=opt.nthreads, no_killer=opt.no_killer)
