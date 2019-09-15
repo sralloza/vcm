7d91b2b0c3223ea1f5589456c91ab2106cc87ef5
sralloza <sralloza@gmail.com>
2019-09-15 12:47:08 +0200

    Add database interface for notifier links

---
+++ vcm/notifier/database.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+from vcm.core.options import Options
+
+
+class DatabaseInterface:
+    def __init__(self):
+        self.connection = sqlite3.connect(Options.DATABASE_PATH.as_posix())
+        self.cursor = self.connection.cursor()
+        self.ensure_table()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.commit()
+        self.close()
+
+    def ensure_table(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS database_links (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                url TEXT UNIQUE NOT NULL
+                )""")
+
+    def commit(self):
+        self.connection.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+
+    def save_link(self, link):
+        try:
+            self.cursor.execute("INSERT INTO database_links VALUES (NULL,?)", [link.url])
+            return True
+        except sqlite3.IntegrityError:
+            return False
+
+
+class DatabaseLinkInterface:
+    @staticmethod
+    def save(link):
+        with DatabaseInterface() as connection:
+            connection.save_link(link)
