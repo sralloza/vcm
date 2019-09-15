59925a967ff8ea5c040975e04c51332b27756e88
sralloza <sralloza@gmail.com>
2019-09-15 16:15:51 +0200

    Fix NotifierSubject

--- vcm/notifier/subject.py
+++ vcm/notifier/subject.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 from vcm.downloader.subject import Subject
@@ -5,20 +6,29 @@ from vcm.notifier.link import NotifierLink
 
 NL = List[NotifierLink]
 
+logger = logging.getLogger(__name__)
+
 
 class NotifierSubject(Subject):
     def __init__(self, name, url, connection, queue):
         super().__init__(name, url, connection, queue)
         self.new_links: NL = []
-        self.determine_new_links()
 
     @classmethod
     def from_subject(cls, subject: Subject):
-        return NotifierSubject(subject.name, subject.url, subject.connection, subject.queue)
+        ns = NotifierSubject(subject.name, subject.url, subject.connection, subject.queue)
+        ns.notes_links = subject.notes_links
+        ns.determine_new_links()
+        return ns
 
     def determine_new_links(self):
+        total = 0
         for link in self.notes_links:
             notifier_link = NotifierLink.from_link(link)
             result = notifier_link.save()
             if result:
-                self.notes_links.append(notifier_link)
+                self.new_links.append(notifier_link)
+                total += 1
+
+        logger.debug('Found %d new links of %s', total, self.name)
+        return total
