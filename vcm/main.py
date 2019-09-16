7f5a86acd3dc36dc01633203eb25a967ce2f6c6f
sralloza <sralloza@gmail.com>
2019-09-16 23:37:36 +0200

    Add show_config option in CLI

--- vcm/main.py
+++ vcm/main.py
@@ -1,12 +1,20 @@
 import argparse
+from enum import Enum
 
+from vcm.core.options import Options
 from vcm.downloader import download
 from vcm.notifier import notify
 
 
+class Command(Enum):
+    notify = 1
+    download = 2
+
+
 def main():
-    # TODO - Test, I don't think it works as it is supposed to
     parser = argparse.ArgumentParser()
+    parser.add_argument('--show-config', action='store_true')
+
     subparsers = parser.add_subparsers(title='commands', dest='command')
 
     downloader_parser = subparsers.add_parser('download')
@@ -20,11 +28,24 @@ def main():
 
     opt = parser.parse_args()
 
-    if opt.command is None:
-        return parser.error('Invalid use: use download or notify')
+    if opt.show_config:
+        data = ['root_folder', 'logs_folder', 'logging_level', 'database_path', 'config_path',
+                'credentials_path']
+        data.sort()
+        message = '\n'.join(
+            ['%s: %s' % (x.replace('_', ' ').strip(), getattr(Options, x.upper())) for x in data])
 
-    elif opt.command == 'download':
+        exit(message)
 
+    try:
+        opt.command = Command(opt.command)
+    except ValueError:
+        try:
+            opt.command = Command[opt.command]
+        except KeyError:
+            return parser.error('Invalid use: use download or notify')
+
+    if opt.command == Command.download:
         if opt.debug:
             import webbrowser
 
@@ -33,7 +54,7 @@ def main():
 
         return download(nthreads=opt.nthreads, no_killer=opt.no_killer)
 
-    elif opt.command == 'notify':
-        return notify('sralloza@gmail.com', not opt.no_icons, nthreads=opt.nthreads)
+    elif opt.command == Command.notify:
+        return notify(['sralloza@gmail.com'], not opt.no_icons, nthreads=opt.nthreads)
     else:
         return parser.error('Invalid command: %r' % opt.command)
