b89d1a1b2c04ddb178ca6ab0804ca28fffa9ca36
sralloza <sralloza@gmail.com>
2019-10-12 12:27:53 +0200

    Show file number in Results class

--- vcm/core/results.py
+++ vcm/core/results.py
@@ -6,34 +6,42 @@ from colorama import Fore
 from .options import Options
 
 
+class Counters:
+    updated = 0
+    new = 0
+
+    @classmethod
+    def count_updated(cls):
+        cls.updated += 1
+
+    @classmethod
+    def count_new(cls):
+        cls.new += 1
+
+
 class Results:
     """Class to manage information."""
+
     print_lock = Lock()
 
     file_lock = Lock()
     result_path = Options.ROOT_FOLDER / 'new-files.txt'
 
     @staticmethod
-    def print_updated(message):
-        """Prints an updated message (yellow) thread-safely.
-
-        Args:
-            message (str): message to print
-
-        """
+    def print_updated(filepath):
+        """Prints an updated message (yellow) thread-safely."""
+        Counters.count_updated()
+        message = "File updated no. %3d: %s" % (Counters.updated, filepath)
         with Results.print_lock:
             print(Fore.LIGHTYELLOW_EX + message + Fore.RESET)
 
         Results.add_to_result_file(message)
 
     @staticmethod
-    def print_new(message):
-        """Prints an new message (green) thread-safely.
-
-        Args:
-            message (str): message to print
-
-        """
+    def print_new(filepath):
+        """Prints an new message (green) thread-safely."""
+        Counters.count_new()
+        message = "New file no. %3d: %s" % (Counters.new, filepath)
         with Results.print_lock:
             print(Fore.LIGHTGREEN_EX + message + Fore.RESET)
 
