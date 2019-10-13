47ff84b7653c4fb6d4b1e573d4d64faaea2874b2
sralloza <sralloza@gmail.com>
2019-10-13 11:04:57 +0200

    Add type control in mapping of FileCache

--- vcm/downloader/filecache.py
+++ vcm/downloader/filecache.py
@@ -8,18 +8,41 @@ from vcm.core.settings import GeneralSettings
 
 class FileCache:
     """File scanner to control file version."""
+
     path = GeneralSettings.root_folder
 
     def __init__(self):
         self.cache = {}
 
     def __contains__(self, item):
+        if not isinstance(item, Path):
+            raise TypeError(
+                "FileCache.__contains__ must be used with Path, not %r"
+                % type(item).__name__
+            )
         return item in self.cache.keys()
 
     def __getitem__(self, item):
+        if not isinstance(item, Path):
+            raise TypeError(
+                "FileCache.__getitem__ must be used with Path, not %r"
+                % type(item).__name__
+            )
         return self.cache[item]
 
     def __setitem__(self, key, value):
+        if not isinstance(key, Path):
+            raise TypeError(
+                "FileCache.__setitem__'s key must be Path, not %r"
+                % type(key).__name__
+            )
+
+        if not isinstance(value, int):
+            raise TypeError(
+                "FileCache.__setitem__'s value must be int, not %r"
+                % type(value).__name__
+            )
+
         self.cache[key] = value
 
     def __len__(self):
@@ -29,7 +52,7 @@ class FileCache:
         """Starts the scanner."""
 
         if not _auto:
-            raise FileCacheError('Use REAL_FILE_CACHE instead')
+            raise FileCacheError("Use REAL_FILE_CACHE instead")
 
         filenames = []
         for elem in os.walk(self.path.as_posix()):
@@ -44,10 +67,10 @@ class FileCache:
 
     def _get_file_length(self, file):
         """Gets the file content length."""
-        with file.open('rb') as file_handler:
+        with file.open("rb") as file_handler:
             length = len(file_handler.read())
 
-        self.cache[file] = length
+        self[file] = length
 
 
 REAL_FILE_CACHE = FileCache()
