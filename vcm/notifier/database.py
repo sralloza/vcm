9c4ce73db0e7ac70089e8bbc200c760153118f4d
sralloza <sralloza@gmail.com>
2020-02-06 12:22:48 +0100

    Insert link's name to links db

--- vcm/notifier/database.py
+++ vcm/notifier/database.py
@@ -39,10 +39,11 @@ class DatabaseInterface:
         data = [
             link.url,
             link.subject.name,
+            link.name,
             datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
         ]
         try:
-            self.cursor.execute("INSERT INTO database_links VALUES (NULL,?,?,?)", data)
+            self.cursor.execute("INSERT INTO database_links VALUES (NULL,?,?,?,?)", data)
             return True
         except sqlite3.IntegrityError:
             return False
