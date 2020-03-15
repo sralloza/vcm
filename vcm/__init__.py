738240bbd66ca052baf0060d6e40998a8f6d3f0d
sralloza <sralloza@gmail.com>
2020-03-16 00:10:54 +0100

    Remove useless imports in vcm.__init__

--- vcm/__init__.py
+++ vcm/__init__.py
@@ -1,12 +1,12 @@
 """File downloader for the Virtual Campus of the Valladolid Unversity."""
-from pathlib import Path
 
-from .core.settings import GeneralSettings
 
 __all__ = []
 
 
 def get_version():
+    from pathlib import Path
+
     version_path = Path(__file__).with_name("VERSION")
     return version_path.read_text()
 
