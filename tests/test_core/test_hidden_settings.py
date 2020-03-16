3b8789ae53cd6bc812bc2381455cbd38ad865912
sralloza <sralloza@gmail.com>
2020-03-16 10:54:50 +0100

    Add test to cover that every settings dict has the same keys

--- tests/test_core/test_hidden_settings.py
+++ tests/test_core/test_hidden_settings.py
@@ -30,3 +30,32 @@ def test_constructors():
 
 def test_setters():
     assert 0, "Not implemented"
+
+
+class TestSettingsDictKeys:
+    def get_keys_from_dicts(self, *dicts):
+        keys = []
+        for dict in dicts:
+            keys += list(dict.keys())
+        return tuple(set(keys))
+
+    def test_settings_dict_keys(self):
+        from vcm.core._settings import defaults, checkers, constructors, setters
+
+        keys = self.get_keys_from_dicts(defaults, checkers, constructors, setters)
+        keys = tuple(set(keys))
+        for key in keys:
+            assert key in defaults
+            assert key in checkers
+            assert key in constructors
+            assert key in setters
+
+            subkeys = self.get_keys_from_dicts(
+                defaults[key], checkers[key], constructors[key], setters[key]
+            )
+
+            for subkey in subkeys:
+                assert subkey in defaults[key]
+                assert subkey in checkers[key]
+                assert subkey in constructors[key]
+                assert subkey in setters[key]
