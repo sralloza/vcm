6cb361ce67d1e7a1fbc17aedae913cea62e9a9bd
sralloza <sralloza@gmail.com>
2020-03-18 17:14:42 +0100

    Add tests for vcm.__init__

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,15 @@
-import pytest
+from unittest import mock
 
 
-@pytest.mark.xfail
-def test_get_version():
-    assert 0, "Not implemented"
+@mock.patch("pathlib.Path")
+def test_get_version(path_m):
+    import vcm
+
+    path_m.return_value.with_name.return_value.read_text.return_value = "testing"
+
+    version = vcm.get_version()
+    assert version == "testing"
+
+    path_m.assert_called_with(vcm.__file__)
+    path_m.return_value.with_name.assert_called_once_with("VERSION")
+    path_m.return_value.with_name.return_value.read_text.assert_called_once()
