48362791e0633a59e985d13e37f28e9cdcb910c5
sralloza <sralloza@gmail.com>
2020-03-19 12:50:11 +0100

    Add tests for main.Command

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,25 @@
+from enum import Enum
+
 import pytest
+
+from vcm.main import Command
 
 
 class TestCommand:
-    @pytest.mark.xfail
+    def test_inherintance(self):
+        assert issubclass(Command, Enum)
+
     def test_attributes(self):
-        assert 0, "Not implemented"
+        assert hasattr(Command, "notify")
+        assert hasattr(Command, "download")
+        assert hasattr(Command, "settings")
+        assert hasattr(Command, "discover")
+
+    def test_types(self):
+        assert isinstance(Command.notify.value, int)
+        assert isinstance(Command.download.value, int)
+        assert isinstance(Command.settings.value, int)
+        assert isinstance(Command.discover.value, int)
 
 
 @pytest.mark.xfail
