db7f2026493b31165ae6134c0c926fbd0bd149e7
sralloza <sralloza@gmail.com>
2020-05-27 16:58:49 +0200

    perf: improve setters for exclude_subjects_ids and section_indexing

--- vcm/core/_settings.py
+++ vcm/core/_settings.py
@@ -8,11 +8,11 @@ from .exceptions import SettingsError
 def exclude_subjects_ids_setter(*args, **kwargs):
     if kwargs.pop("force", False):
         exclude_list = args[0]
-        for element in exclude_list:
+        for index, element in enumerate(exclude_list):
             if not isinstance(element, int):
                 raise TypeError(
-                    "%r element of exclude-subjects-ids must be int, not %s"
-                    % (element, type(element).__name__)
+                    "Element %d (%r) of exclude-subjects-ids must be int, not %s"
+                    % (index, element, type(element).__name__)
                 )
         return exclude_list
     raise SettingsError("general.exclude-subjects-ids can't be set using the CLI.")
@@ -21,11 +21,11 @@ def exclude_subjects_ids_setter(*args, **kwargs):
 def section_indexing_setter(*args, **kwargs):
     if kwargs.pop("force", False):
         exclude_list = args[0]
-        for element in exclude_list:
+        for index, element in enumerate(exclude_list):
             if not isinstance(element, int):
                 raise TypeError(
-                    "%r element of section-indexing must be int, not %s"
-                    % (element, type(element).__name__)
+                    "Element %d (%r) of section-indexing must be int, not %s"
+                    % (index, element, type(element).__name__)
                 )
         return exclude_list
     raise SettingsError("download.section-indexing can't be set using the CLI")
