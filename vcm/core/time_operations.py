1e6a7d77ce7e61a099314cf418063ede127cf96d
sralloza <sralloza@gmail.com>
2020-05-27 16:50:41 +0200

    chore: improve implementation of seconds_to_str

--- vcm/core/time_operations.py
+++ vcm/core/time_operations.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from .exceptions import InvalidLanguageError
 
 ALPHABET = {
@@ -12,9 +14,24 @@ ALPHABET = {
 }
 
 
+@dataclass
+class Language:
+    day: str
+    hour: str
+    minute: str
+    second: str
+    s: str
+    join: str
+
+
+LANGUAGES = {
+    "abbr": {k: Language(*v) for k, v in ALPHABET["abbr"].items()},
+    "default": {k: Language(*v) for k, v in ALPHABET["default"].items()},
+}
+
+
 def seconds_to_str(seconds, abbreviated=False, integer=None, language="en"):
     """Returns seconds extended as string.
-
     Args:
         seconds (int, float): number of seconds.
         abbreviated (bool): use the abbreviated form or not.
@@ -29,102 +46,76 @@ def seconds_to_str(seconds, abbreviated=False, integer=None, language="en"):
 
     try:
         if abbreviated is True:
-            day_str, hour_str, minute_str, second_str, final_s, s_last = ALPHABET[
-                "abbr"
-            ][language]
+            language = LANGUAGES["abbr"][language]
         else:
-            day_str, hour_str, minute_str, second_str, final_s, s_last = ALPHABET[
-                "default"
-            ][language]
+            language = LANGUAGES["default"][language]
     except KeyError:
         raise InvalidLanguageError(f"{language!r} is not a valid language")
 
     try:
         int(seconds)
     except OverflowError:
-        return "infinite %s%s" % (day_str, final_s)
+        return "infinite %s%s" % (language.day, language.s)
 
-    if integer is True:
-        seconds = int(seconds)
+    day, hour, minute, second = split_seconds(seconds, integer=integer, days=True)
 
-    before = ", "
-    s_last = " " + s_last + " "
-    has_before = [False, False, False, False]
-    has_not_zero = [0, 0, 0, 0]
+    day_part = gen_part(day, language.day, language.s, abbreviated)
+    hour_part = gen_part(hour, language.hour, language.s, abbreviated)
+    minute_part = gen_part(minute, language.minute, language.s, abbreviated)
+    second_part = gen_part(second, language.second, language.s, abbreviated)
 
-    day, hour, minute, second = _split_seconds(seconds, integer=integer, days=True)
+    output = join_parts(language.join, day_part, hour_part, minute_part, second_part)
 
-    if second:
-        last = 4
-    elif minute:
-        last = 3
-    elif hour:
-        last = 2
-    elif day:
-        last = 1
-    else:
-        last = 4
-
-    if day:
-        has_before[1] = True
-        has_before[2] = True
-        has_before[3] = True
-
-        has_not_zero[0] = 1
-    if hour:
-        has_before[2] = True
-        has_before[3] = True
-
-        has_not_zero[1] = 1
-    if minute:
-        has_before[3] = True
-
-        has_not_zero[2] = 1
-    if second:
-        has_not_zero[3] = 1
-
-    only_one = sum(has_not_zero) == 1
-    ret = ""
-
-    if day:
-        ret += "{} {}".format(day, day_str)
-        if day - 1:
-            ret += final_s
-    if hour:
-        if last == 2 and not only_one:
-            ret += s_last
-        elif has_before[1]:
-            ret += before
-        ret += "{} {}".format(hour, hour_str)
-        if hour - 1:
-            ret += final_s
-    if minute:
-        if last == 3 and not only_one:
-            ret += s_last
-        elif has_before[2]:
-            ret += before
-        ret += "{} {}".format(minute, minute_str)
-        if minute - 1:
-            ret += final_s
-    if second:
-        if last == 4 and not only_one:
-            ret += s_last
-        elif has_before[3]:
-            ret += before
-        ret += "{} {}".format(second, second_str)
-        if second - 1:
-            ret += final_s
-
-    if second == minute == hour == day == 0:
-        return "0 %s%s" % (second, final_s)
-
-    return ret
+    if not output:
+        return gen_part(
+            0, language.second, language.s, abbreviated, force_output=True
+        )
+    return output
 
 
-def _split_seconds(total_seconds, days=False, integer=None):
-    """Transforma segundos en horas,minutos y segundos."""
+def gen_part(int_part, str_part, possible_s, is_abbr, force_output=False):
+    if not int_part and not force_output:
+        return None
 
-    # total_seconds = int(total_seconds)
+    output = str(int_part)
+    if not is_abbr:
+        output += " "
+
+    output += str_part
+
+    if int_part - 1 and not is_abbr:
+        output += possible_s
+    return output
+
+
+def join_parts(join_str, *string_parts):
+    string_parts = [x for x in string_parts if x]
+
+    if not string_parts:
+        return None
+
+    if len(string_parts) == 1:
+        return string_parts[0]
+
+    output = ", ".join(string_parts[:-1])
+    return output + " " + join_str + " " + string_parts[-1]
+
+
+def split_seconds(total_seconds, days=False, integer=None):
+    """[summary]
+    Args:
+        total_seconds (int or float): total number of seconds.
+        days (bool, optional): If true, it will return a 4-length tuple,
+            including the days. Defaults to False.
+        integer (bool or None, optional): If True, the seconds returned
+            will be an integer. If False, no conversion will be made. If
+            None and the remaining seconds are 0, it will return the
+            seconds (0) as an int, regardless that it was int or float.
+            Defaults to None.
+    Returns:
+        tuple: 3-length tuple: (hours, seconds, minutes). If days is True,
+        it will be a 4-length tuple: (days, hours, seconds, minutes).
+    """
 
     total_minutes = total_seconds // 60
     seconds = total_seconds % 60
@@ -134,14 +125,14 @@ def _split_seconds(total_seconds, days=False, integer=None):
     seconds = round(seconds, 2)
 
     if integer is True:
-        seconds = int(seconds)
+        seconds = round(seconds, 0)
     elif integer is None:
         if seconds == int(seconds):
             seconds = int(seconds)
 
     if not days:
         return hours, minutes, seconds
-    else:
-        days = hours // 24
-        hours = hours % 24
-        return days, hours, minutes, seconds
+
+    days = hours // 24
+    hours = hours % 24
+    return days, hours, minutes, seconds
