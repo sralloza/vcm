28b0b2466d086c823813d4bf74621dc2641e2d3b
sralloza <sralloza@gmail.com>
2020-08-12 19:27:06 +0200

    test: fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,16 @@
-from ast import literal_eval
+from functools import lru_cache
+import json
 import os
 from pathlib import Path
 
 import pytest
-import toml
+from ruamel.yaml import YAML
 
 
+@lru_cache(maxsize=10)
 def get_settings_defaults():
-    path = Path(__file__).parent.with_name("vcm").joinpath("core/_settings.py")
-
-    outfile = []
-    with path.open("rt", encoding="utf-8") as infile:
-        copy = False
-        for line in infile:
-            if line.strip() == "defaults = {":
-                copy = True
-                outfile.append("{\n")
-                continue
-            if line.strip() == "}":
-                if copy:
-                    outfile.append(line)
-                copy = False
-                continue
-            if copy:
-                outfile.append(line)
-
-    return literal_eval("".join(outfile))
+    path = Path(__file__).parent.with_name("vcm").joinpath("core/defaults.json")
+    return json.loads(path.read_text())
 
 
 # IMPORTED FROM vcm.core._settings
@@ -42,29 +27,31 @@ CREDENTIALS_DEFAULT = {
 }
 
 
-test_settings_path: Path = Path.home() / "test-vcm-settings.toml"
-test_credentials_path: Path = Path.home() / "test-vcm-credentials.toml"
+test_settings_path: Path = Path.home() / "test-vcm-settings.yaml"
+test_credentials_path: Path = Path.home() / "test-vcm-credentials.yaml"
 test_root_folder = Path.home() / "vcm-test-data"
 
 
 def pytest_configure():
-    SETTINGS_DEFAULTS["general"]["root-folder"] = test_root_folder.as_posix()
-    SETTINGS_DEFAULTS["notify"]["email"] = "testing@example.com"
+    SETTINGS_DEFAULTS["root-folder"] = test_root_folder.as_posix()
+    SETTINGS_DEFAULTS["email"] = "testing@example.com"
+
+    yaml = YAML()
 
     # if not test_settings_path.exists():
     with test_settings_path.open("wt") as file_handler:
-        toml.dump(SETTINGS_DEFAULTS, file_handler)
+        yaml.dump(SETTINGS_DEFAULTS, file_handler)
 
     # if not test_credentials_path.exists():
     with test_credentials_path.open("wt") as file_handler:
-        toml.dump(CREDENTIALS_DEFAULT, file_handler)
+        yaml.dump(CREDENTIALS_DEFAULT, file_handler)
 
     os.environ["TESTING"] = "True"
 
 
 @pytest.fixture(scope="session", autouse=True)
 def check_settings():
-    from vcm.core._settings import defaults
+    defaults = get_settings_defaults()
 
     assert isinstance(defaults, dict)
 
